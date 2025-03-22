f03e6945070bf19ef29398d2d84f68a488a736ae
Konig-Varorson <73151188+Konig-Varorson@users.noreply.github.com>
2025-03-22 05:25:44 -0400

    Add files via upload

---
+++ source/cg_local.h
@@ -0,0 +1,13 @@
+// Copyright (c) ZeniMax Media Inc.
+// Licensed under the GNU General Public License 2.0.
+
+// g_local.h -- local definitions for game module
+#pragma once
+
+#include "bg_local.h"
+
+extern cgame_import_t cgi;
+extern cgame_export_t cglobals;
+
+#define FRAME_TIME_S cgi.frame_time_s
+#define FRAME_TIME_MS cgi.frame_time_ms
